476e3e171901e29e97bbd0fa42c14675de46d2d5
mcsco <michael@cloudadrift.com>
2024-01-12 10:11:11 -0600

    script checks current distro. DLs correct pkg and installs... Not verified

--- myprinter.sh
+++ myprinter.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 
 # Making sure you have unarchiver
-
-sudo apt install unzip cups
-
-# Retrieve discord tar.gz file 
+distro=`lsb_release -is`
 
 cd ~/Downloads/
 
-wget "https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.3-1.gz"
+if [ $distro = "Debian" ] || [ $ditro = "Ubuntu" ]; then
+    sudo apt install unzip cups
+    # Retrieve brother gz file
+    wget "https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.3-1.gz"
+    gunzip linux-brprinter-installer-*.*.*-*.gz
+    sudo bash linux-brprinter-installer-2.2.3-1 MFC-L2750DW
 
-gunzip linux-brprinter-installer-*.*.*-*.gz
+elif [ $distro = "Fedora" ] || [ $distro = "NobaraLinux" ]; then
+    wget "https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.3-1.gz"
+    gunzip linux-brprinter-installer-*.*.*-*.gz
+    sudo bash linux-brprinter-installer-2.2.3-1 MFC-L2750DW
+fi
 
-sudo bash linux-brprinter-installer-2.2.3-1 mfc7440n
+# Clean up
+mkdir -vp ~/Downloads/brprinter
+mv brscan* ~/Downloads/brprinter/
+mv linux-brprinter* ~/Downloads/brprinter/
+mv mfcl2750dw* ~/Downloads/brprinter/
+mv uninstaller* ~/Downloads/brprinter/
 
-
+cd ~/
