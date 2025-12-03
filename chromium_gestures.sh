08f0fa09b28f25f16b3c165bb44e7a1a3364d1f8
mcsco <michael@cloudadrift.com>
2025-12-02 20:07:42 -0600

    added brave to chromium_gestures.sh script. fixed KDE shortcut settings popup appears every chromium based browsers launch.

--- chromium_gestures.sh
+++ chromium_gestures.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 cp /usr/share/applications/chromium.desktop $HOME/.local/share/applications/
+cp /usr/share/applications/brave-browser.desktop $HOME/.local/share/applications/
 
 echo "Add"
 echo "--enable-features=TouchpadOverscrollHistoryNavigation"
+echo "--disable-features=GlobalShortcutsPortal"
 echo "at the end of &"
 $EDITOR .local/share/applications/chromium.desktop
+$EDITOR .local/share/applications/brave-browser.desktop
